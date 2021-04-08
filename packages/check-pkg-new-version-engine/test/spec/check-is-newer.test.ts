b30af12e4c1d9b5fd5fe375f7e1e13b1a5b90fca
Joel Chen <xchen@walmartlabs.com>
2021-04-08 14:11:46 -0700

    fix test

--- packages/check-pkg-new-version-engine/test/spec/check-is-newer.test.ts
+++ packages/check-pkg-new-version-engine/test/spec/check-is-newer.test.ts
@@ -1,16 +1,18 @@
 /* eslint-disable no-magic-numbers */
 
 import { internalCheckIsNewer } from "../../src/check-is-newer";
+import { CheckResult } from "../../src/types";
 test("internalCheckIsNewer", () => {
-  const runCheck = (v1: string, v2: string, shouldBe: boolean) => {
+  const runCheck = (v1: string, v2: string, shouldBe: CheckResult) => {
     const e1 = internalCheckIsNewer({ name: "test", version: v1 }, { latest: v2 }, "latest");
-    expect(e1).toBe(shouldBe);
+    expect(e1.isNewer).toBe(shouldBe.isNewer);
+    expect(e1.version).toBe(shouldBe.version);
   };
 
   [
-    ["1.0.0", "1.1.0", true],
-    ["1.0.0", "1.0.0", false],
-    ["1.1.0", "1.0.0", false],
+    ["1.0.0", "1.1.0", { isNewer: true, version: "1.1.0" }],
+    ["1.0.0", "1.0.0", { isNewer: false }],
+    ["1.1.0", "1.0.0", { isNewer: false }],
   ].forEach((d: any[]) => {
     runCheck(d[0], d[1], d[2]);
   });
