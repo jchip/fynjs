f4a45d88a6bf4918cd832cd51cf5916ed2bbce38
Joel Chen <joel123@gmail.com>
2025-12-27 22:37:30 -0800

    FJM-13: modernize init-package to TypeScript + dual-mode ESM/CJS

--- packages/init-package/test/test.ts
+++ packages/init-package/test/test.ts
@@ -1,5 +1,5 @@
-import { runInitPackage } from "../src";
-import { describe, it, expect } from "@jest/globals";
+import { runInitPackage } from "../src/index.ts";
+import { describe, it, expect } from "vitest";
 
 describe("init-package", function () {
   it("should have runInitPackage", () => {
