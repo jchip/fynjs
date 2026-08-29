8df507c2ad514d7ee1bad3125cf79c71bd4bc92e
Joel Chen <joel123@gmail.com>
2026-08-29 01:11:56 -0700

    FJM-84: fynpo-cli use optional-require 2.x instead of hand-rolled MODULE_NOT_FOUND check

--- packages/fynpo-cli/bin/fynpo.ts
+++ packages/fynpo-cli/bin/fynpo.ts
@@ -1,22 +1,24 @@
 #!/usr/bin/env node
 
-import requireAt from "require-at";
+import { makeOptionalRequire } from "optional-require";
 
-try {
-  requireAt(process.cwd(), "fynpo/dist/fynpo-cli");
-} catch (err) {
-  if (err.code === "MODULE_NOT_FOUND") {
+const optionalRequire = makeOptionalRequire(process.cwd());
+
+optionalRequire("fynpo/dist/fynpo-cli", {
+  notFound: () => {
     console.error(`ERROR: Unable to find the fynpo module from dir ${process.cwd()}
 
 Please make sure you are in a fynpo monorepo and you have installed fynpo
 at its top level.
 `);
-  } else {
+    process.exit(1);
+  },
+  fail: (err: Error) => {
     console.error(
       `Fail to load fynpo for your monorepo from dir ${process.cwd()}
 `,
       err
     );
+    process.exit(1);
   }
-  process.exit(1);
-}
+});
